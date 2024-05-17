8f386342777f13d3d40138deded592938a529292
Malcolm Rajeli <malcolmjeremiahrajeli@gmail.com>
2024-05-17 07:03:19 +0000

    Updated: Turbo stream works

---
+++ app/controllers/homepage_controller.rb
@@ -0,0 +1,22 @@
+class HomepageController < ApplicationController
+  def show
+    @text = params[:text]
+
+    respond_to do |format|
+      format.html
+      format.turbo_stream do
+        render turbo_stream: [
+          turbo_stream.update(
+            "text-container",
+            partial: 'homepage/text',
+            locals: { text: @text }
+          ),
+          turbo_stream.update(
+            "navbar",
+            partial: 'shared/nav'
+          ),
+        ]
+      end
+    end
+  end
+end
