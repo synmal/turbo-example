ca449146cc3839b3804867cf5c0c6b445d61fde1
Malcolm Rajeli <malcolmjeremiahrajeli@gmail.com>
2024-05-18 03:22:03 +0000

    Added FA

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,4 @@ pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js"
+pin "@fortawesome/fontawesome-free", to: 'https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.5.2/js/all.js' # @6.5.2
